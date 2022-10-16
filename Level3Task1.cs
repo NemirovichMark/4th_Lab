75b11fcc102ad64d9089736045477365f8e70584
Emil Gafarov <113677882+gafarovExe@users.noreply.github.com>
2022-10-16 22:02:10 +0300

    Update Level3Task1.cs

--- Level3Task1.cs
+++ Level3Task1.cs
@@ -10,20 +10,14 @@ class HelloWorld
         Console.WriteLine("Enter the second matrix size: ");
         int m = Convert.ToInt32(Console.ReadLine());
         List<List<int>> matrix = new List<List<int>>();
+        List<List<int>> buffer = new List<List<int>>();
         var A = new List<int>();
         var B = new List<int>();
         var C = new List<int>();
         var mas = new List<int>();
-        int sum = 0;
-        string s = "0";
         int[] str = new int[n];
-        int b=0;
-        int v1 = 0, v2 = 0;
-        int k = 0;
-        int x;
-        int t = 0;
         Console.WriteLine("Enter the matrix: ");
-        for (int i = 0; i < n ; i++)
+        for (int i = 0; i < n; i++)
         {
             matrix.Add(new List<int>());
             string[] line = Console.ReadLine().Split(" ");
@@ -32,23 +26,35 @@ class HelloWorld
                 matrix[i].Add(Convert.ToInt32(line[j]));
             }
         }
-        for (int i=0; i<n; i++)
+        for (int i = 0; i < n; i++)
         {
             A.Add(matrix[i].Min());
         }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < A.Count(); j++)
+            {
+                if (A[j] == A.Max()) { B.Add(j); A[j] = A.Min() - 1; break; }
+            }
+        }
         for (int i=0; i<n; i++)
         {
-            for (int j=0; j<A.Count(); j++)
+            buffer.Add(new List<int>());
+            for (int j=0; j < matrix[0].Count(); j++)
             {
-                if (A[j] == A.Max()) { B.Add(j); A[j] = A.Min() - 1 ; break; }
-            } 
+                buffer[i].Add(matrix[i][j]);
+            }
+        }
+        for (int i=0; i<n; i++)
+        {
+            matrix[i] = buffer[B[i]];
         }
         Console.WriteLine("New matrix: ");
         for (int i = 0; i < matrix.Count(); i++)
         {
             for (int j = 0; j < matrix[0].Count(); j++)
             {
-                Console.Write(matrix[B[i]][j] + " ");
+                Console.Write(matrix[i][j] + " ");
             }
             Console.WriteLine();
         }
