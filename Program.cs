f2f4ff4ecfa508468f3e1ec9488519dc5fa791ac
MichailSitnikov <113028484+MichailSitnikov@users.noreply.github.com>
2022-12-22 22:17:26 +0300

    Lab4 fixed
    Hope it`ll be fine 0_0

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Reflection;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using System.Xml.Schema;
 
 namespace Lab_4
@@ -180,7 +182,7 @@ namespace Lab_4
             int.TryParse(Console.ReadLine(), out int m2);
 
 
-            M = new int[k2, k2];
+            M = new int[k2, m2];
             for (int i = 0; i < k2; i++)
                 for (int j = 0; j < m2; j++)
                     M[i, j] = random.Next(50);
@@ -226,41 +228,35 @@ namespace Lab_4
             m = 7;
             M = new int[k, m];
             for (int i = 0; i < k; i++)
-                for (int j = 0; j < m; j++)
-                    M[i, j] = random.Next(-10, 10);
-            for (int i = 0; i < k; i++)
             {
                 for (int j = 0; j < m; j++)
                 {
+                    M[i, j] = random.Next(-50, 50);
                     Console.Write(M[i, j] + "\t");
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("\n");
-            iM = 0;
+            int min = M[1, 0];
+            int minj = 0;
+            for (int j = 0; j < m; j++)
+            {
+                if (Math.Abs(M[1, j]) < min)
+                {
+                    min = M[1, j];
+                    minj = j;
+                }
+            }
+            Console.WriteLine($"The minimal element is {min}");
+
+            m -= 1;
             for (int i = 0; i < k; i++)
             {
-                int minm = M[i, 0];
-                for (int j = 0; j < m; j++)
+                for (int j = minj + 1; j < m; j++)
                 {
-                    if (j == 2 && Math.Abs(minm) > Math.Abs(M[i, j]))
-                    {
-                        minm = M[i, j];
-                        iM++;
-                    }
+                    M[i, j] = M[i, j + 1];
                 }
             }
-            m -= 1;
-            if (iM < m - 1)
-            {
-                for (int i = iM + 1; i < k; i++)
-                {
-                    for (int j = 0; j < m; j++)
-                    {
-                        M[i, j] = M[i, j + 1];
-                    }
-                }
-            }
+            Console.WriteLine("Here`s our changed array:");
             for (int i = 0; i < k; i++)
             {
                 for (int j = 0; j < m; j++)
@@ -451,81 +447,62 @@ namespace Lab_4
             Console.WriteLine("Level 3!!!");
             #region 3.1
             Console.WriteLine("Task 3.1");
-            M = new int[7, 5];
-            for (int i = 0; i < M.GetLength(0); i++)
-            {
-                for (int j = 0; j < M.GetLength(1); j++)
-                M[i, j] = random.Next(-10, 10);
-
-            }
-            MM = new int[7, 2];
-            int [,] MMM = new int[7, 5];
-            for (int i = 0; i < M.GetLength(0); i++)
-            {
-                for (int j = 0; j < M.GetLength(1); j++)
-                {
-                    Console.WriteLine(M[i,j] + "\t" );
-                }
-            }
+            k = 7;
+            m = 5;
+            M = new int [k, m];
+            MM = new int [k, m];
+            int index;
+            double[] minimal = new double[7];
+            double[] copied = new double[7];
+            int[] ind = new int[7];
+            bool flag = true;
             for (int i = 0; i < 7; i++)
             {
-                int minI = i;
-                int minJ = 0;
-                int minM = M[i, 0];
+                double MinI = double.MaxValue;
+                Console.WriteLine($"Write down 5 elems in i line");
                 for (int j = 0; j < 5; j++)
                 {
-                    if (M[i, j] < minM)
+                    if (int.TryParse(Console.ReadLine(), out int s)) { M[i, j] = s; MM[i, j] = s; }
+                    else
                     {
-                        minM = M[i, j];
-                        minI = i;
-                        minJ = jk;
+                        Console.WriteLine("wrong");
+                        flag = false;
+                        break;
                     }
+                    if (s < MinI) { MinI = s; index = j; }
                 }
-                k = 0;
-                q = 0;
-                MM[q, k] = minI;
-                MM[q, k + 1] = minM;
-                q += 1;
+                if (!flag) break;
+                minimal[i] = MinI;
+                copied[i] = MinI;
             }
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 7; i++)
             {
-                for (int j = 1; j < 6; j++)
+                double MaxI = minimal[i];
+                int IND = i;
+                for (int j = i + 1; j < 7; j++)
                 {
-
-                    if (MM[i, 1] < MM[j + 1, 1])
-                    {
-                        m = MM[i, 1];
-
-                        MM[i, 1] = MM[j + 1, 1];
-
-                        MM[j + 1, 1] = m;
-
-                        m = MM[i, 0];
-
-                        MM[i, 0] = MM[j + 1, 0];
-
-                        MM[j + 1, 0] = m;
-                    }
+                    if (minimal[j] > MaxI) { MaxI = minimal[j]; IND = j; }
                 }
+                ind[i] = Array.IndexOf(copied, MaxI);
+                minimal[IND] = minimal[i];
+                minimal[i] = MaxI;
+                copied[Array.IndexOf(copied, MaxI)] = double.MinValue;
             }
-
-
-
             for (int i = 0; i < 7; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    MMM[i, j] = M[MM[i, 0], j];
+                    M[i, j] = MM[ind[i], j];
                 }
             }
             for (int i = 0; i < 7; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    Console.WriteLine(MMM[i, j] + "\t");
+                    Console.Write("{0,4}", M[i, j]);
                 }
+                Console.WriteLine();
             }
-
             #endregion
             #region 3.2
             Console.WriteLine("Task 3.2");
@@ -658,181 +635,203 @@ namespace Lab_4
             #endregion
             #region 3.8
             Console.WriteLine("Task 3.8");
-            M = new int[7, 5];
-            MM = new int[7, 5];
-            int fl, fl2, fl3;
-            for (int i = 0; i < M.GetLength(0); i++)
+            k = 7;
+            m = 5;
+            M = new int[k, m];
+            Random ran = new Random();
+            for (int i = 0; i < k; i++)
             {
-                for (int j = 0; j < M.GetLength(1); j++)
+                for (int j = 0; j < m; j++)
                 {
-                    int.TryParse(Console.ReadLine(), out k);
-                    M[i, j] = k;
+                    M[i, j] = ran.Next(-20, 20);
                 }
             }
-            MMM = new int[7, 2];
-            q = 0;
-            for (int i = 0; i < 7; i++)
+            Console.WriteLine();
+            for (int i = 0; i < k; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(M[i, j] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+            int[] b = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                int x = 0;
+                for (int j = 0; j < m; j++)
                 {
                     if (M[i, j] > 0)
                     {
-                        q += 1;
+                        x++;
                     }
                 }
-                MMM[i, 0] = i;
-
-                MMM[i, 1] = q;
-                q = 0;
+                b[i] = x;
             }
-            for (int i = 0; i < 7; i++)
+            Console.WriteLine();
+            foreach (int u in b)
             {
-                maxM = MMM[i, 1];
-                fl = i;
-
-                for (int j = 1 + i; j < 7; j++)
+                Console.Write(u + " ");
+            }
+            Console.WriteLine();
+            int f = 0;
+            while (f < b.Length - 1)
+            {
+                if (b[f] < b[f + 1])
                 {
-                    if (MMM[j, 1] > maxM)
+                    int temp = b[f];
+                    b[f] = b[f + 1];
+                    b[f + 1] = temp;
+                    for (int i = 0; i < m; i++)
                     {
-                        maxM = MMM[j, 1];
-
-                        fl = j;
+                        int g = M[f, i];
+                        M[f, i] = M[f + 1, i];
+                        M[f + 1, i] = g;
+                    }
+                    if (f > 0)
+                    {
+                        f--;
                     }
                 }
-
-                fl2 = MMM[i, 0];
-
-                MMM[i, 0] = fl;
-
-                MMM[fl, 0] = fl2;
-
-                fl3 = MMM[i, 1];
-
-                MMM[i, 1] = maxM;
-
-                MMM[fl, 1] = fl3;
-
-            }
-            for (int i = 0; i < 7; i++)
-            {
-                fl = MMM[i, 0];
-
-                for (int j = 0; j < 5; j++)
+                else
                 {
-                    MM[i, j] = M[fl, j];
+                    f++;
                 }
             }
-
-
-            for (int i = 0; i < 7; i++)
+            Console.WriteLine();
+            for (int i = 0; i < k; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < m; j++)
                 {
-                    Console.WriteLine(MM[i, j]);
+                    Console.Write(M[i, j] + "\t");
+
                 }
+                Console.WriteLine();
             }
             #endregion
             #region 3.10
             Console.WriteLine("Task 3.10");
-            Console.WriteLine("Enter the amount of lines in matrix");
-            int.TryParse(Console.ReadLine(), out k);
-            Console.WriteLine("Enter the amount of columns in matrix");
-            int.TryParse(Console.ReadLine(), out q);
-            M = new int[k, q];
-            for (int i = 0; i < k; i++)
-                for (int j = 0; j < q; j++)
-                    M[i, j] = random.Next(50);
-            for (int i = 0; i < k; i++)
+            Console.WriteLine("Write down Lines and Columns in array");
+            flag = true;
+            if (int.TryParse(Console.ReadLine(), out k) && k > 0 && int.TryParse(Console.ReadLine(), out m) && m > 0)
             {
-                for (int j = 0; j < q; j++)
+                M = new int[k, m];
+                for (int i = 0; i < k; i++)
                 {
-                    Console.Write(M[i, j] + "\t");
-                }
-            }
-            Console.WriteLine("\n");
-            for (int i = 0; i < k; i++)
-            {
-                for (int j = 0; j < q - 1; j++)
-                {
-                    for (int s = j; s < q; s++)
+                    Console.WriteLine($"Write down {m} elems in {i} line");
+                    for (int j = 0; j < m; j++)
                     {
-                        if (i % 2 == 0)
-                        {
-                            if (M[i, j] < M[i, s])
-                            {
-                                p = M[i, j]; M[i, j] = M[i, s]; M[i, s] = p;
-                            }
-                        }
+                        if (int.TryParse(Console.ReadLine(), out int s)) { M[i, j] = s; }
                         else
                         {
-                            if (M[i, j] > M[i, s])
-                            {
-                                p = M[i, j]; M    [i, j] = M[i, s]; M[i, k] = p;
-                            }
+                            Console.WriteLine("Wrong");
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (!flag) break;
+                }
+                for (int i = 0; i < k; i++)
+                {
+                    int[] Z = new int[m];
+                    if (i % 2 == 0)
+                    {
+                        for (q = 0; q < m; q++)
+                        {
+                            Z[q] = M[i, q];
+                        }
+                        Array.Sort(Z); Array.Reverse(Z);
+                        for (q = 0; q < m; q++)
+                        {
+                            M[i, q] = Z[q];
+                        }
+                    }
+                    else
+                    {
+                        for (q = 0; q < m; q++)
+                        {
+                            Z[q] = M[i, q];
+                        }
+                        Array.Sort(Z);
+                        for (q = 0; q < m; q++)
+                        {
+                            M[i, q] = Z[q];
                         }
                     }
                 }
-            }
-            for (int i = 0; i < k; i++)
-            {
-                for (int j = 0; j < q; j++)
+                for (int i = 0; i < k; i++)
                 {
-                    Console.Write(M[i, j] + "\t");
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write("{0,4}", M[i, j]);
+                    }
+                    Console.WriteLine();
                 }
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+                return;
             }
             #endregion
             #region 3.11
             Console.WriteLine("Task 3.11");
-            Console.WriteLine("Enter the amount of lines in matrix");
-            int.TryParse(Console.ReadLine(), out k);
-            Console.WriteLine("Enter the amount of columns in matrix");
-            int.TryParse(Console.ReadLine(), out q);
-            M = new int[k, q];
-            List<int> ok = new List<int>();
-
-            for (int i = 0; i < k; i++)
+            Console.WriteLine("Write down Lines and Columns of the Array");
+            flag = true;
+            if (int.TryParse(Console.ReadLine(), out k) && k > 0 && int.TryParse(Console.ReadLine(), out m) && m > 0)
             {
-                bool fla = false;
-                for (int j = 0; j < q; j++)
+                M = new int[k, m];
+                for (int i = 0; i < k; i++)
                 {
-                    M[i, j] = random.Next(-50, 50);
-                    if (M[i, j] == 0)
+                    Console.WriteLine($"Write down {m} elems in i lines");
+                    for (int j = 0; j < m; j++)
                     {
-                        fla = true;
+                        if (int.TryParse(Console.ReadLine(), out q)) { M[i, j] = q; }
+                        else
+                        {
+                            Console.WriteLine("Wrong");
+                            flag = false;
+                            break;
+                        }
                     }
+                    if (!flag) break;
                 }
-                if (fla == true)
+                int IND = 0; 
+                int IN = 0;
+                MM = new int[k, m];
+                for (int i = 0; i < k; i++)
                 {
-                    ok.Add(i);
+                    flag = true;
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (M[i, j] == 0) { flag = false; break; }
+                    }
+                    if (!flag) continue;
+                    else
+                    {
+                        for (int j = 0; j < m; j++)
+                        {
+
+                            MM[IN, j] = M[i, j];
+                        }
+                        IN += 1;
+                    }
+
+                }
+                for (int i = 0; i < IN; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write("{0,4}", MM[i, j]);
+                    }
+                    Console.WriteLine();
                 }
             }
-            for (int i = 0; i < k; i++)
+            else
             {
-                for (int j = 0; j < q; j++)
-                {   
-                    Console.Write(M[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            q = 0;
-            a = ok.Count();
-            for (int i = 0; i < k; i++)
-            {
-                if (q == a)
-                {
-                    q = 0;
-                }
-                else if (i == ok[q])
-                {
-                    q += 1;
-                    continue;
-                }
-                for (int j = 0; j < q; j++)
-                {
-                    Console.Write(M[i, j] + "\t");
-                }
-                Console.WriteLine();
+                Console.WriteLine("Wrong");
+                return;
             }
             #endregion
             #endregion
