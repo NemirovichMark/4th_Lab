a43493e74676cc5c1291e10d43f3d4917d9510d5
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-27 12:42:26 +0300

    Update Level1_Task29.cs

--- Level1_Task29.cs
+++ Level1_Task29.cs
@@ -2,27 +2,28 @@ using System;
 using System.Linq; 
 class HelloWorld { 
     static void Main() { 
-        try{ 
-            double[,] a = new double[5,7]; 
-            double[,] b = new double[5,6]; 
+        try{
+            int n=5,m=7,row=1;
+            double[,] a = new double[n,m]; 
+            double[,] b = new double[n,m-1]; 
             Random rnd = new Random(); 
-            for(int i=0; i<5; i++) 
-                for(int j=0; j<7; j++) 
+            for(int i=0; i<n; i++) 
+                for(int j=0; j<m; j++) 
                     a[i,j] = rnd.Next()%20; 
              
            Console.Write("Your random matrix:\n"); 
-            for(int i=0; i<5; i++){ 
-                for(int j=0; j<7; j++){ 
+            for(int i=0; i<n; i++){ 
+                for(int j=0; j<m; j++){ 
                     Console.Write("{0} ",a[i,j]); 
                 } 
                 Console.WriteLine(); 
             } 
             int min=0; 
-            for(int i=0; i<7; i++) if(Math.Abs(a[1,i])<Math.Abs(a[1,min])) min=i; 
-            if(min==6){ 
+            for(int i=0; i<m; i++) if(Math.Abs(a[row,i])<Math.Abs(a[row,min])) min=i; 
+            if(min==m-1){ 
                 Console.Write("\nThe answer matrix:\n"); 
-                for(int i=0; i<5; i++){ 
-                    for(int j=0; j<7; j++){ 
+                for(int i=0; i<n; i++){ 
+                    for(int j=0; j<m; j++){ 
                         Console.Write("{0} ",a[i,j]); 
                     } 
                     Console.WriteLine(); 
@@ -30,17 +31,17 @@ class HelloWorld {
                 return; 
             } 
             int cy=0; 
-            for(int i=0; i<5; i++){ 
+            for(int i=0; i<n; i++){ 
                 cy=0; 
-                for(int j=0; j<7; j++){ 
+                for(int j=0; j<m; j++){ 
                     if(j==min+1) continue; 
                     b[i,cy++] = a[i,j]; 
                 } 
             } 
                      
             Console.Write("\nThe answer matrix:\n"); 
-            for(int i=0; i<5; i++){ 
-                for(int j=0; j<6; j++){ 
+            for(int i=0; i<n; i++){ 
+                for(int j=0; j<m-1; j++){ 
                     Console.Write("{0} ",b[i,j]); 
                 } 
                 Console.WriteLine(); 
