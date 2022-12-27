c7e4e8dae2f38a848be9e613a236ad5ff31c3e56
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-27 13:33:35 +0300

    Update Level1_Task31.cs

--- Level1_Task31.cs
+++ Level1_Task31.cs
@@ -2,37 +2,40 @@ using System;
 using System.Linq;  
 class HelloWorld {  
     static void Main() {  
-        try{  
-            double[,] a = new double[5,8];  
-            double[] b = new double[5]{3,1,4,1,5}; 
+        try{ 
+            int n=5,m=8,row=4;
             Random rnd = new Random();  
-            for(int i=0; i<5; i++)  
-                for(int j=0; j<7; j++)  
+            double[,] a = new double[n,m];  
+            double[] b = new double[n];
+            for(int i=0; i<n; i++) b[i]=rnd.Next()%10;
+            
+            for(int i=0; i<n; i++)  
+                for(int j=0; j<m-1; j++)  
                     a[i,j] = rnd.Next()%20; 
               
            Console.Write("Your random matrix:\n");  
-            for(int i=0; i<5; i++){  
-                for(int j=0; j<7; j++){  
+            for(int i=0; i<n; i++){  
+                for(int j=0; j<m-1; j++){  
                     Console.Write("{0} ",a[i,j]);  
                 }  
                 Console.WriteLine();  
             } 
             Console.Write("\nThe array to insert: "); 
-            for(int i=0; i<5; i++) Console.Write($" {b[i]}"); 
+            for(int i=0; i<n; i++) Console.Write($" {b[i]}"); 
             Console.Write("\n"); 
             int min=0; 
-            for(int i=0; i<7; i++) 
-                if(a[4,i]<a[4,min]) min=i; 
-            for(int i=0; i<5; i++){ 
-                for(int j=7; j>min+1; j--){ 
+            for(int i=0; i<m-1; i++) 
+                if(a[row,i]<a[row,min]) min=i; 
+            for(int i=0; i<n; i++){ 
+                for(int j=m-1; j>min+1; j--){ 
                     a[i,j-1] = a[i,j] + a[i,j-1] - (a[i,j]=a[i,j-1]); 
                 } 
                 a[i,min+1]=b[i]; 
             } 
              
             Console.Write("\nThe answer matrix:\n");  
-            for(int i=0; i<5; i++){  
-                for(int j=0; j<8; j++){  
+            for(int i=0; i<n; i++){  
+                for(int j=0; j<m; j++){  
                     Console.Write("{0} ",a[i,j]);  
                 }  
                 Console.WriteLine();  
