6771f10d8a53305d705c584ff290950a27ee8370
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-27 13:37:25 +0300

    Update Level1_Task6.cs

--- Level1_Task6.cs
+++ Level1_Task6.cs
@@ -3,27 +3,30 @@ using System.Linq;
 class HelloWorld {
     static void Main() {
         try{
-            double sum=0;
-            double[,] a = new double[4,7]{{3,1,4,1,5,9,2},
-                                          {6,5,3,5,8,9,7},
-                                          {9,3,2,3,8,4,6},
-                                          {2,6,5,3,5,8,9}};
-            double[] mins = new double[4];
-            Console.Write("Do you want enter your numbers or want to see this program working with a initialized with numbers of Pi? (Pi/My): ");
+            int n=4,m=7;
+            Random rnd = new Random();
+            double[,] a = new double[n,m];
+            double[] mins = new double[n];
+            Console.Write("Do you want enter your numbers or want to see this program working initialized with random numbers? (Rnd/My): ");
             string inp = Console.ReadLine();
             if(inp=="My"){
-                for(int i=0; i<4;i++)
-                    for(int j=0; j<7; j++){
+                for(int i=0; i<n;i++)
+                    for(int j=0; j<m; j++){
                         a[i,j] = double.Parse(Console.ReadLine());
                         if(j==0) mins[i]=a[i,j];
                         else if(a[i,j]<mins[i]) mins[i]=a[i,j];
                     }
             }
             else{
-                for(int i=0; i<4;i++)
-                    for(int j=0; j<7; j++)
+                for(int i=0; i<n;i++){
+                    for(int j=0; j<m; j++){
+                        a[i,j] = rnd.Next()%10;
+                        Console.Write($"{a[i,j]} ");
                         if(j==0) mins[i]=a[i,j];
                         else if(a[i,j]<mins[i]) mins[i]=a[i,j];
+                    }
+                    Console.WriteLine();
+                }
             }
             Console.Write("Answer:");
             foreach(var i in mins)
