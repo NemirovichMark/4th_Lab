a5b1e8575f590316828654c45ced6847dd2ef3f5
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-27 12:37:12 +0300

    Update Level3_Task1.cs

--- Level3_Task1.cs
+++ Level3_Task1.cs
@@ -1,41 +1,47 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
-class HelloWorld {
+class HelloWorld{
     static void SwapRows(double[,] a, int first, int second){
-        for(int i=0; i<5; i++)
+        for(int i=0; i<a.GetLength(1); i++)
             a[first,i] = a[first,i] + a[second,i] - (a[second,i]=a[first,i]);
     }
-    static void Main() {
-        try{
-            double[,] a = new double[7,5];
-            Random rnd = new Random();
-            for(int i=0; i<7; i++)
-                for(int j=0; j<5; j++)
-                    a[i,j] = rnd.Next()%10;
-            Console.WriteLine("Your random matrix:");
-            for(int i=0; i<7; i++){
-                for(int j=0; j<5; j++)
-                    Console.Write($"{a[i,j]} ");
-                Console.WriteLine();
+    static void Main (){
+        int n=7,m=5;
+        double[,] a = new double[n,m];
+        Random rnd = new Random();
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                a[i,j]=rnd.Next()%21-10;
+        Console.Write("There is your random matrix:\n");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                Console.Write($"{a[i,j]} ");
             }
-            int[] mins = new int[7]{0,0,0,0,0,0,0};
-            for(int i=0; i<7; i++)
-                for(int j=0; j<5; j++)
-                    mins[i]=(a[i,j]<a[i,mins[i]])?j:mins[i];
-            for(int j=0; j<6; j++)
-                for(int i=0; i<6; i++)
-                    if(a[i,mins[i]]<a[i+1,mins[i+1]]){
-                        SwapRows(a,i,i+1);
-                        mins[i] = mins[i+1] + mins[i] - (mins[i+1]=mins[i]);
-                    }
-            Console.WriteLine("\nYour answer matrix:");
-            for(int i=0; i<7; i++){
-                for(int j=0; j<5; j++)
-                    Console.Write($"{a[i,j]} ");
-                Console.WriteLine();
-            }
+            Console.WriteLine();
         }
-        catch{}
+        
+        int[] mins = new int[n];
+        for(int i=0; i<n; i++) mins[i]=0;
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                mins[i]=(a[i,j]<a[i,mins[i]])?j:mins[i];
+        bool flag = true;
+        for(int j=0; flag; j++){
+            flag = false;
+            for(int i=0; i<n-1; i++)
+                if(a[i,mins[i]]<a[i+1,mins[i+1]]){
+                    flag = true;
+                    SwapRows(a,i,i+1);
+                    mins[i] = mins[i+1] + mins[i] - (mins[i+1]=mins[i]);
+                    break;
+                }
+        }
+        
+        Console.WriteLine("There is your answer matrix:");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                Console.Write($"{a[i,j]} ");
+            }
+            Console.WriteLine();
+        }
     }
 }
