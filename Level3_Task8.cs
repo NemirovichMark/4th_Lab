10e27208311c307f34a215f77d4370c1b021f6ce
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-27 12:31:10 +0300

    Update Level3_Task8.cs

--- Level3_Task8.cs
+++ Level3_Task8.cs
@@ -1,37 +1,44 @@
 using System;
 class HelloWorld{
     static void SwapRows(double[,] a, int first, int second){
-        for(int i=0; i<5; i++)
+        for(int i=0; i<a.GetLength(1); i++)
             a[first,i] = a[first,i] + a[second,i] - (a[second,i]=a[first,i]);
     }
     static void Main (){
-        double[,] a = new double[7,5];
+        int n=7,m=5;
+        double[,] a = new double[n,m];
         Random rnd = new Random();
-        for(int i=0; i<7; i++)
-            for(int j=0; j<5; j++)
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
                 a[i,j]=rnd.Next()%21-10;
         Console.Write("There is your random matrix:\n");
-        for(int i=0; i<7; i++){
-            for(int j=0; j<5; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 Console.Write($"{a[i,j]} ");
             }
             Console.WriteLine();
         }
         
-        int[] pos = new int[7]{0,0,0,0,0,0,0};
-            for(int i=0; i<7; i++)
-                for(int j=0; j<5; j++)
-                    pos[i]+=(a[i,j]>0)?1:0;
-            for(int j=0; j<6; j++)
-                for(int i=0; i<6; i++)
-                    if(pos[i]<pos[i+1]){
-                        SwapRows(a,i,i+1);
-                        pos[i] = pos[i+1] + pos[i] - (pos[i+1]=pos[i]);
-                    }
+        int[] pos = new int[n];
+        for(int i=0; i<n; i++) pos[i]=0;
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                pos[i]+=(a[i,j]>0)?1:0;
+        bool flag = true;
+        for(int j=0; flag; j++){
+            flag = false;
+            for(int i=0; i<n-1; i++)
+                if(pos[i]<pos[i+1]){
+                    flag = true;
+                    SwapRows(a,i,i+1);
+                    pos[i] = pos[i+1] + pos[i] - (pos[i+1]=pos[i]);
+                    break;
+                }
+        }
         
         Console.WriteLine("There is your answer matrix:");
-        for(int i=0; i<7; i++){
-            for(int j=0; j<5; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 Console.Write($"{a[i,j]} ");
             }
             Console.WriteLine();
