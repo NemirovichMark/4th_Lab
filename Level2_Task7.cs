71ac3d711c68fae32a92f7e3a3159e0d2ee31c71
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-27 13:22:38 +0300

    Update Level2_Task7.cs

--- Level2_Task7.cs
+++ Level2_Task7.cs
@@ -4,27 +4,28 @@ using System.Collections.Generic;
 class HelloWorld {
     static void Main() {
         try{
-            double[,] a = new double[6,6];
+            int n=6;
+            double[,] a = new double[n,n];
             Random rnd = new Random();
-            for(int i=0; i<6; i++)
-                for(int j=0; j<6; j++)
+            for(int i=0; i<n; i++)
+                for(int j=0; j<n; j++)
                     a[i,j] = rnd.Next()%10;
             Console.WriteLine("Your random matrix:");
-            for(int i=0; i<6; i++){
-                for(int j=0; j<6; j++)
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++)
                     Console.Write($"{a[i,j]} ");
                 Console.WriteLine();
             }
             Console.WriteLine();
             int max=0;
-            for(int i=0; i<6; i++)
+            for(int i=0; i<n; i++)
                 max=(a[i,i]>a[max,max])?i:max;
-            for(int i=0; i<6; i++)
-                for(int j=0; j<6; j++)
+            for(int i=0; i<n; i++)
+                for(int j=0; j<n; j++)
                     if(i<max&&j>i) a[i,j]=0;
             Console.WriteLine("Your answer matrix:");
-            for(int i=0; i<6; i++){
-                for(int j=0; j<6; j++)
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++)
                     Console.Write($"{a[i,j]} ");
                 Console.WriteLine();
             }
