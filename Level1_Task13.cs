c2989ba35d253aa5a052debe12ba59fbdb73a4eb
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-27 12:20:14 +0300

    Update Level1_Task13.cs

--- Level1_Task13.cs
+++ Level1_Task13.cs
@@ -3,19 +3,23 @@ using System.Linq;
 class HelloWorld {
     static void Main() {
         try{
-            double[,] a = new double[5,5]{{3,1,4,1,5},
-                                          {6,5,3,22,8},
-                                          {9,8,2,14,8},
-                                          {2,6,4,3,3},
-                                          {2,7,9,8,0}};
+            int n=5,column=3;
+            double[,] a = new double[n,n];
+            Random rnd = new Random();
             int max=0;
-            for(int i=0; i<5; i++)
-                if(a[i,i]>a[max,max]) max=i;
-            for(int i=0; i<5; i++)
-                a[i,3] = a[i,3] + a[i,max] - (a[i,max]=a[i,3]);
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    a[i,j] = rnd.Next()%10;
+                    Console.Write($"{a[i,j]} ");
+                    if(i==j&&a[i,j]>a[max,max]) max=i;
+                }
+                Console.WriteLine();
+            }
+            for(int i=0; i<n; i++)
+                a[i,column] = a[i,column] + a[i,max] - (a[i,max]=a[i,column]);
             Console.Write("The answer matrix:\n");
-            for(int i=0; i<5; i++){
-                for(int j=0; j<5; j++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
                     Console.Write("{0} ",a[i,j]);
                 }
                 Console.WriteLine();
