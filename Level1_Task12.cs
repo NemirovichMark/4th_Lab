84a50062f949efc3ff8fc59a5e05802f0c668150
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-27 12:13:32 +0300

    Update Level1_Task12.cs

--- Level1_Task12.cs
+++ Level1_Task12.cs
@@ -3,25 +3,30 @@ using System.Linq;
 class HelloWorld {
     static void Main() {
         try{
-            double[,] a = new double[6,7]{{3,1,4,1,5,9,2},
-                                          {6,5,3,5,8,9,7},
-                                          {9,3,2,3,8,4,6},
-                                          {2,6,4,3,3,8,3},
-                                          {2,7,9,5,0,2,8},
-                                          {8,4,1,9,7,1,5}};
-            double[,] b = new double[5,6];
+            int n=6,m=7;
+            Random rnd = new Random();
+            double[,] a = new double[n,m];
+            double[,] b = new double[n-1,m-1];
             int mx=0,my=0;
-            for(int i=0; i<6; i++)
-                for(int j=0; j<7; j++)
+            for(int i=0; i<n; i++){
+                for(int j=0; j<m; j++){
+                    a[i,j] = rnd.Next()%10;
+                    Console.Write($"{a[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+                    
+            for(int i=0; i<n; i++)
+                for(int j=0; j<m; j++)
                     if(a[mx,my]<a[i,j]){
                         mx=i;
                         my=j;
                     }
             int cx=0,cy=0;
-            for(int i=0; i<6; i++){
+            for(int i=0; i<n; i++){
                 if(i==mx) continue;
                 cy=0;
-                for(int j=0; j<7; j++){
+                for(int j=0; j<m; j++){
                     if(j==my) continue;
                     b[cx,cy]=a[i,j];
                     cy++;
@@ -29,8 +34,8 @@ class HelloWorld {
                 cx++;
             }
             Console.Write("The answer matrix:\n");
-            for(int i=0; i<5; i++){
-                for(int j=0; j<6; j++){
+            for(int i=0; i<n-1; i++){
+                for(int j=0; j<m-1; j++){
                     Console.Write("{0} ",b[i,j]);
                 }
                 Console.WriteLine();
